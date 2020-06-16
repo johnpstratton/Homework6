04e92a735e3522a4603931510e43a2459ebcc0ca
John_Stratton <john.p.stratton@gmail.com>
2020-06-16 13:29:00 -0700

    first push with all files

---
+++ exercise110.py
@@ -0,0 +1,11 @@
+# An integer, n , is said to be perfect when the sum of all of the proper divisors of n is equal to   n . 
+# For example, 28 is a perfect number because:
+#  Its proper divisors are 1, 2, 4, 7 and 14, and 1 + 2 + 4 + 7 + 14 = 28.
+
+# Write a function that determines whether or not a positive integer is perfect. 
+# Your function will take one parameter. 
+# If that parameter is a perfect number then your function will return true. 
+# Otherwise it will return false. 
+# In addition, write a main program that uses your function to:
+# Identify and display all of the perfect numbers between 1 and 10,000. 
+# Import your solution to Exercise 109 when completing this task.
