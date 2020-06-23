a6e26ae442d3230165e7763e8c5bfb9df51bc10b
John_Stratton <john.p.stratton@gmail.com>
2020-06-23 09:20:31 -0700

    exercise 110 complete

--- exercise110.py
+++ exercise110.py
@@ -9,3 +9,29 @@
 # In addition, write a main program that uses your function to:
 # Identify and display all of the perfect numbers between 1 and 10,000. 
 # Import your solution to Exercise 109 when completing this task.
+
+def PerfectNum(num):
+    divisor = num
+    values = []
+    while divisor > 0:
+        solution = num %divisor
+        if solution == 0:
+            values.append(divisor)
+        divisor -= 1
+    for x in values:
+        if x == num:
+            values.remove(x)
+        elif sum(values) == num:
+            return True
+        else:
+            return False
+    
+perfectnumbers = []
+
+for x in range(1, 10001):
+    if PerfectNum(x) == True:
+        perfectnumbers.append(x)
+
+print(perfectnumbers)
+
+
