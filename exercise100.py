4f3a70e7e01b4367cb9b23d5e350b78a5688f055
John_Stratton <john.p.stratton@gmail.com>
2020-06-23 08:18:52 -0700

    Commit before renaming repo

--- exercise100.py
+++ exercise100.py
@@ -7,3 +7,31 @@
 
 # Include a main program that reads a month and year from the user and displays the number of days in that month. 
 # You may find your solution to Exercise 57 helpful when solving this problem.
+days = {
+    1 : 31,
+    2 : 28,
+    3 : 31,
+    4 : 30,
+    5 : 31,
+    6 : 30,
+    7 : 31,
+    8 : 31,
+    9 : 30,
+    10 : 31,
+    11 : 30,
+    12 : 31
+}
+def NumofDays(month, year):
+    day = 0
+    for x in days:
+        if (x == month and x != days[2]):
+            day += days[x]
+        elif ((x == month and month == days[2]) and year %4 != 0):
+            day += days[x]
+        elif ((x == month and month == days[2]) and year %4 == 0):
+            day = 29
+            return day
+    print(day)
+month = int(input("Please enter a month between 1-12: "))
+year = int(input("Please enter a year in a four digit format (i.e. 2020): "))
+NumofDays(month, year)
